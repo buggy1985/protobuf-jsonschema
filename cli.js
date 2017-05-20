e4718ef8533f4abab0d4512871a3d6f14e6e3229
liuweibo <liuweibo@deepglintdeMacBook-Pro-2.local>
2017-05-20 13:07:41 +0800

    [mod] allow multi-proto gen

--- cli.js
+++ cli.js
@@ -6,15 +6,30 @@ var yaml = require('js-yaml');
 
 commander
   .version(require('./package.json').version)
-  .arguments('<file> [model]')
+  .arguments('<files...>')
+  .option('-m, --model [model]', 'compile given model, default all', '')
   .option('-f, --format [format]', 'output format: json or yaml [json]', 'json')
-  .action(function(file, model) {
+  .action(function(files) {
     var format = commander.format || 'json';
-    var result = compile(file, model);
+    var model = commander.model || '';
+    var results = [];
+    files.forEach(function(file){
+        var result = compile(file, model);
+        results.push(result);
+    })
+    var r = {"definitions": {}};
+
+    results.forEach(function(result){
+        var keys = Object.keys(result["definitions"]);
+        for (var i = 0, length = keys.length; i < length; ++i) {
+            var key = keys[i];
+            r["definitions"][key] = result["definitions"][key];
+        }
+    });
     
     if (format === 'json')
-      process.stdout.write(JSON.stringify(result, false, 2) + '\n');
+      process.stdout.write(JSON.stringify(r, false, 2) + '\n');
     else if (format === 'yaml')
-      process.stdout.write(yaml.dump(result, { noRefs: true }));
+      process.stdout.write(yaml.dump(r, { noRefs: true }));
   })
   .parse(process.argv);
