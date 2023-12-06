b3f311b72846e01226b91541da55c1b28743dbb3
Peter Bagi <peter.bagi@gmail.com>
2023-12-06 19:03:59 +0100

    load specific imports from a specified folder, e.g. for google/protobuf/*

--- index.js
+++ index.js
@@ -4,41 +4,50 @@ var primitive = require('./types');
 var fs = require('fs');
 var path = require('path');
 
-function Compiler(filename) {
+function Compiler(filename, opts) {
   this.messages = {};
   this.enums = {};
-  this.schema = this.open(filename);
+  this.schema = this.open(filename, opts);
 }
 
-Compiler.prototype.open = function(filename) {
+Compiler.prototype.open = function (filename, opts) {
   if (!/\.proto$/i.test(filename) && !fs.existsSync(filename)) {
     filename += '.proto';
   }
 
   var schema = parseSchema(fs.readFileSync(filename, 'utf-8'));
   this.visit(schema, schema.package || '');
-  
-  schema.imports.forEach(function(i) {
+
+  schema.imports.forEach(function (i) {
+    if (opts.remapImports) {
+      for (var key in opts.remapImports) {
+        if (i.includes(key)) {
+          this.open(path.resolve(opts.remapImports[key], i));
+          return;
+        }
+      }
+    }
+
     this.open(path.resolve(path.dirname(filename), i));
   }, this);
-  
+
   return schema;
 };
 
 /**
  * Visits a schema in the tree, and assigns messages and enums to the lookup tables.
  */
-Compiler.prototype.visit = function(schema, prefix) {
+Compiler.prototype.visit = function (schema, prefix) {
   if (schema.enums) {
-    schema.enums.forEach(function(e) {
+    schema.enums.forEach(function (e) {
       e.id = prefix + (prefix ? '.' : '') + (e.id || e.name);
       this.enums[e.id] = e;
       this.visit(e, e.id);
     }, this);
   }
-  
+
   if (schema.messages) {
-    schema.messages.forEach(function(m) {
+    schema.messages.forEach(function (m) {
       m.id = prefix + (prefix ? '.' : '') + (m.id || m.name);
       this.messages[m.id] = m;
       this.visit(m, m.id);
@@ -51,24 +60,24 @@ Compiler.prototype.visit = function(schema, prefix) {
  * compiles just that type and its dependencies. Otherwise,
  * compiles all types in the file.
  */
-Compiler.prototype.compile = function(type) {
+Compiler.prototype.compile = function (type) {
   this.root = {
     definitions: {},
     used: {}
   };
-  
+
   if (type) {
     this.resolve(type, '');
   } else {
-    this.schema.messages.forEach(function(message) {
+    this.schema.messages.forEach(function (message) {
       this.resolve(message.id, '');
     }, this);
-    
-    this.schema.enums.forEach(function(e) {
+
+    this.schema.enums.forEach(function (e) {
       this.resolve(e.id, '');
     }, this);
   }
-  
+
   delete this.root.used;
   return this.root;
 };
@@ -77,34 +86,34 @@ Compiler.prototype.compile = function(type) {
  * Resolves a type name at the given path in the schema tree.
  * Returns a compiled JSON schema.
  */
-Compiler.prototype.resolve = function(type, from, base, key, description="") {
+Compiler.prototype.resolve = function (type, from, base, key, description = "") {
   if (primitive[type]) {
     return _.clone(primitive[type])
   }
-  
+
   var lookup = from.split('.');
   for (var i = lookup.length; i >= 0; i--) {
     var id = lookup.slice(0, i).concat(type).join('.');
-  
+
     // If this type was used before, move it from inline to a reusable definition
     if (this.root.used[id] && !this.root.definitions[id]) {
       var k = this.root.used[id];
       this.root.definitions[id] = k[0][k[1]];
       k[0][k[1]] = { $ref: '#/definitions/' + id };
     }
-  
+
     // If already defined, reuse
     if (this.root.definitions[id])
       return { $ref: '#/definitions/' + id };
-  
+
     // Compile the message or enum
     var res;
     if (this.messages[id])
       res = this.compileMessage(this.messages[id]);
-  
+
     if (this.enums[id])
       res = this.compileEnum(this.enums[id]);
-  
+
     if (res) {
       // If used, or at the root level, make a definition
       if (this.root.used[id] || !base) {
@@ -114,22 +123,22 @@ Compiler.prototype.resolve = function(type, from, base, key, description="") {
           res.description = description
         }
       }
-      
+
       // Mark as used
       if (base && !this.root.used[id])
         this.root.used[id] = [base, key];
-    
+
       return res;
     }
   }
-  
+
   throw new Error('Could not resolve ' + type);
 };
 
 /**
  * Compiles and assigns a type
  */
-Compiler.prototype.build = function(type, from, base, key, description='') {
+Compiler.prototype.build = function (type, from, base, key, description = '') {
   var res = this.resolve(type, from, base, key, description);
   if (primitive[type]) {
     res.description = description
@@ -149,21 +158,21 @@ Compiler.prototype.build = function(type, from, base, key, description='') {
 /**
  * Compiles a protobuf enum to JSON schema
  */
-Compiler.prototype.compileEnum = function(enumType, root) {
+Compiler.prototype.compileEnum = function (enumType, root) {
   var res = {
     title: enumType.name,
     type: 'string',
     description: enumType.description,
     enum: Object.keys(enumType.values)
   };
-  
+
   return res;
 };
 
 /**
  * Compiles a protobuf message to JSON schema
  */
-Compiler.prototype.compileMessage = function(message, root) {
+Compiler.prototype.compileMessage = function (message, root) {
   var res = {
     title: message.name,
     description: message.description,
@@ -171,8 +180,8 @@ Compiler.prototype.compileMessage = function(message, root) {
     properties: {},
     required: []
   };
-  
-  message.fields.forEach(function(field) {
+
+  message.fields.forEach(function (field) {
     if (field.map) {
       if (field.map.from !== 'string')
         throw new Error('Can only use strings as map keys at ' + message.id + '.' + field.name);
@@ -181,15 +190,15 @@ Compiler.prototype.compileMessage = function(message, root) {
       if (field.hasOwnProperty('description')) {
         desc = field.description
       }
-      
+
       var f = res.properties[field.name] = {
         type: 'object',
         additionalProperties: null,
         description: desc,
       };
-      
+
       this.build(field.map.to, message.id, f, 'additionalProperties');
-    } else {      
+    } else {
 
       var desc = "";
       if (field.hasOwnProperty('description')) {
@@ -202,23 +211,23 @@ Compiler.prototype.compileMessage = function(message, root) {
           items: null,
           description: desc,
         };
-        
+
         this.build(field.type, message.id, f, 'items');
       } else {
         this.build(field.type, message.id, res.properties, field.name, desc);
       }
     }
-    
+
     if (field.required)
       res.required.push(field.name);
   }, this);
-  
+
   if (res.required.length === 0)
     delete res.required;
   return res;
 };
 
-module.exports = function(filename, model) {
-  var compiler = new Compiler(filename);
+module.exports = function (filename, model, opts = {}) {
+  var compiler = new Compiler(filename, opts);
   return compiler.compile(model);
 };
